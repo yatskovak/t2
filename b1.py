8e362bdc8731ddc9e2ef517d4ccfc20f1d775859
Alexandr Yatskov <yatskovak@gmail.com>
2016-12-05 11:57:49 +0300

    AAA commit 4

---
+++ b1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#   filename ==  b1.py
+
+print (" == ФЫВА  QQQ ==" )
+print (" commit 4 " )
---
+++ c1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#   filename ==  c1.py
+
+print (" == ЯЧСМИ  QQQ ==" )
+print (" commit 4 " )
