c8da179fe1ae803bdb06c9ae7b88d93b303c9832
Alexandr Yatskov <yatskovak@gmail.com>
2016-12-07 18:55:24 +0300

    add brbr2.py  branch=br2

---
+++ brbr2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-  
+
+# -*- coding: cp1251 -*-              ### for windows    
+
+import  os, time
+os.system("clear")
+name_arch = time.strftime('%Y%m%d_%H%M%S')  +'.7z'
+### name_arch = time.strftime('%Y%m%d') + target_name +time.strftime('%H%M%S') +'.7z'
+### os.system("pause") #for  DOS-Windows
+print ("-1-")
+aaa = input("Press Enter  -") # for Python3      and raw_input('How long to wait: ')   # for Python2     
+print (aaa, "   ---   ", name_arch)
