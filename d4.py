fc0cf92a8c95f8f11123768c310bcb12fb666156
Alexandr_Yatskov_C77 <it-mag@mail.ru>
2022-12-14 16:16:04 +0300

    D4 COMMENT2

--- d4.py
+++ d4.py
@@ -1,1 +1,10 @@
+
 print("d4 == ")
+# Импортируйте pandas как pd
+import pandas as pd
+
+# Прочитайте и загрузите данные Excel-файла в переменную survey_responses
+survey_responses = pd.read_excel("ddd444b.xlsx")
+
+# Распечатайте первых несколько строк
+print(survey_responses.head())
