1e7151bcc243e69fce5f9939ed260bd5f87b5efc
Alexandr_Yatskov_C77 <it-mag@mail.ru>
2022-12-14 19:13:03 +0300

    D4 COMMENT3

--- a1.py
+++ a1.py
@@ -5,7 +5,20 @@ print (" == ФЫВА  QQQ ==i ---1" )
 print (" commit 3 " )
 print (" commit a1 ==1  " )
 
+def is_anagram(a,b):
+    c =  a[::-1]
+    print(f"\n\n==========  a={a}         b={b} =========== {c == b }")
+    #print(  c == b )
+    return c == b
 
 
-print (" aaa = ФЫВА  QQQ == ЙЙФФЮЮ ==" )
-print (  "a1   ==    3a" )
+a = "qaz"
+b = "zaq"
+assert is_anagram(a,b) is True
+#is_anagram(a,b)
+print("====="*12)
+a = "aaa"
+b = "zaq"
+#is_anagram(a,b)
+assert is_anagram(a,b) is False
+
